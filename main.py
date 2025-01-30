893d1416480ae31017767ec988716384a5e8ba00
Vaslav <2key@rambler.ru>
2025-01-30 13:33:37 +0500

    fixture

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-def check(num):
-    return num % 2 == 0
+import sqlite3
 
-def is_palindrom(s):
-    return s == s[::-1]
+def init_db():
+    conn = sqlite3.connect(':memory:')
+    cursor = conn.cursor()
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    age INTEGER)
+    ''')
+    return conn
+
+def add_user(conn, name, age):
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO users (name, age) VALUES (?, ?)''', (name, age))
+    conn.commit()
+
+def get_user(conn, name):
+    cursor = conn.cursor()
+    cursor.execute('''SELECT * FROM users WHERE name=?''', (name,))
+    return cursor.fetchone()
+
--- test.py
+++ test.py
@@ -1,39 +1,15 @@
 import pytest
-from main import check, is_palindrom
+from main import init_db, add_user, get_user
 
-def test_is_palindrom_true():
-    assert is_palindrom('madam') == True
+@pytest.fixture
+def db_conn():
+    conn = init_db()
+    yield conn
+    conn.close()
 
-def test_is_palindrom_false():
-    assert is_palindrom('bot') == False
-
-@pytest.mark.parametrize("test_input, expected", [
-    ('level', True),
-    ('python', False),
-    ('racecar', True),
-    ('', True),
-])
-
-def test_isPalindrome(test_input, expected):
-    assert is_palindrom(test_input) == expected
-
-
-def test_check_2():
-    assert check(6) == True
-
-def test_check_1():
-    assert check(5) == False
-
-@pytest.mark.parametrize('num,expected', [
-    (2, True),
-    (5, False),
-    (0, True),
-    (56, True),
-    (-3, False),
-    (-100, True)
-])
-
-def test_check_with_param(num, expected):
-   assert check(num) == expected
+def test_add_or_get_user(db_conn):
+    add_user(db_conn, "Sasha", 30)
+    user = get_user(db_conn, "Sasha")
+    assert user == (1, "Sasha", 30)
 
 
