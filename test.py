4dfac7d3b46261c60c1a895894b1c894bf0823e7
Vaslav <2key@rambler.ru>
2025-01-30 12:15:20 +0500

    pytest

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import unittest
 
 class TestMath(unittest.TestCase):
-    def test_add(self):
+    def test_add_1(self):
         self.assertEqual(1 + 1, 2)
-    def test_subtract(self):
+    def test_sub_1(self):
         self.assertEqual(3 - 2, 1)
 
 
